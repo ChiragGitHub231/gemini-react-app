be7708c682f72dd54a700097c770b52d8d221562
ChiragGitHub231 <chiragbhundiya433@gmail.com>
2024-05-29 13:05:43 +0530

    Styling the page, search-section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-const App = () => {
-  return (
-    <div className="">
+import { useState } from "react";
 
+const App = () => {
+  const [error, setError] = useState("")
+
+  return (
+      <div className="app">
+        <p>What do you want to know?
+        <button className="surprise">Surprise me</button>
+        </p>
+
+        <div className="input-container">
+          <input
+            value={""}
+            placeholder="When is Chrismas...?"
+            onChange={""}
+          />
+          {!error && <button>Ask me</button>}
+          {error && <button>Clear</button>}
+        </div>
+
+        {error && <p>{error}</p>}
+        <div className="search-result">
+          <div key={""}>
+            <p className="answer"></p>
+          </div>
+        </div>
     </div>
   );
 }
