0d66f14178f9b6fa287631acf915ff94bc677450
ChiragGitHub231 <chiragbhundiya433@gmail.com>
2024-05-29 18:25:44 +0530

    Create Server and Use Google Gen API

--- src/App.js
+++ src/App.js
@@ -2,20 +2,60 @@ import { useState } from "react";
 
 const App = () => {
   const [error, setError] = useState("")
+  const [value, setValue] = useState("")
+  const [chatHistory, setChatHistory] = useState([])
+
+  const surpriseOptions = [
+    'Who won the world cup 2023?',
+    'Where does pizza come from?',
+    'What is the population of India?'
+  ]
+
+  const surprise = () => {
+    const randomValue = surpriseOptions[Math.floor(Math.random() * surpriseOptions.length)]
+    setValue(randomValue)
+  }
+
+  const getResponse = async () => {
+    if(!value) {
+      setError("Error! Please ask a question!")
+      return
+    }
+
+    try {
+      const options = {
+        method: 'POST',
+        body: JSON.stringify({
+          history: chatHistory,
+          message: value
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+
+      const response = await fetch('http://localhost:8000/gemini', options)
+      const data = response.text()
+      console.log(data)
+    } catch(error) {
+      console.log(error)
+      setError("Something went wrong! Please try again later.")
+    }
+  }
 
   return (
       <div className="app">
         <p>What do you want to know?
-        <button className="surprise">Surprise me</button>
+        <button className="surprise" onClick={surprise} disabled={!chatHistory}>Surprise me</button>
         </p>
 
         <div className="input-container">
           <input
-            value={""}
+            value={value}
             placeholder="When is Chrismas...?"
-            onChange={""}
+            onChange={(e) => setValue(e.target.value)}
           />
-          {!error && <button>Ask me</button>}
+          {!error && <button onClick={getResponse}>Ask me</button>}
           {error && <button>Clear</button>}
         </div>
 
